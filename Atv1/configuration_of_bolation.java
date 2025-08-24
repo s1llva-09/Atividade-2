e66c7877d330de71b71b47ce89ef6584a4e376a0
pJota <xink.kr@gmail.com>
2025-08-24 17:53:51 -0300

    Projeto Java inicial

---
+++ Atv1/configuration_of_bolation.java
@@ -0,0 +1,12 @@
+package bolao;
+
+public class configuration_of_bolation {
+    public String cor;
+    
+    public String getCor() {
+        return cor;
+    }
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+}
